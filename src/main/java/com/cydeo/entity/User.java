24aa3e8c957a042a8b5e01c7b189580cc5674003
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-09-25 19:03:33 -0400

    Ticketing

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,20 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class User extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private boolean enabled;
+    private String phone;
+    private Role role;
+    private Gender gender;
+
+}
